df7d45f247061de73f0aa160ce6b537cb66bc861
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-05 17:31:07 +0200

    HW1

--- HW1/src/Task3.java
+++ HW1/src/Task3.java
@@ -3,14 +3,12 @@ public class Task3 {
         int a = 1;
         int b = 2;
         // Результат до обміну
-        System.out.println("До обміну") ;
-        System.out.println("a = " + a) ;
-        System.out.println("b = " + b) ;
-
-        // Обмін значень
+        System.out.println("До обміну");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+// Обмін значень
         a = a^b^(b = a);
         System.out.println("Після обміну");
-        System.out.printLn(a = "+ a + " b = "  + b);
-
+        System.out.println("a = "+ a + ", b = " + b);
     }
 }
