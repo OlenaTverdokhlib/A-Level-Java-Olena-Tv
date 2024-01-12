c63f1ce1b6fb1b540d214f69e42fcead315a8e46
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-12 19:14:49 +0200

    HW1

---
+++ HW/src/ua/tverdokhlib/hw3/Task3.java
@@ -0,0 +1,4 @@
+package ua.tverdokhlib.hw3;
+
+public class Task3 {
+}
--- HW1/src/Task2.java
+++
@@ -1,8 +0,0 @@
-public class Task2 {
-    public static void main(String[] args) {
-        String імя = "Olena";
-        int зріст = 160;
-        double вага = 56.2;
-        System.out.println( "Імя=" + імя + "; Зріст=" + зріст + "; Вага=" + вага );
-    }
-}
