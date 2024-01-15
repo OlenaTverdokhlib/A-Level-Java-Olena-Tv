69ab150f09251fdfdc6c09d3557abaa1b4a281df
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-15 15:52:25 +0200

    HW1

--- HW/src/ua/tverdokhlib/hw2/Task2.java
+++ HW/src/ua/tverdokhlib/hw2/Task2.java
@@ -6,9 +6,11 @@ public class Task2 {
         double x2 = 3, y2 = 5;
         double x3 = 6, y3 = 7;
         double area = triangleArea(x1, y1, x2, y2, x3, y3);
-       System.out.println("Площа трикутника=" + area);
-        }
-    public static double triangleArea(double x1, double y1, double x2, double y2, double x3, double y3){return
-            0.5 * Math.abs((x1 -x2)*(y1 + y2) +(x2 - x3)*(y2 + y3) + (x3 - x1)*(y3 + y1));
+        System.out.println("Площа трикутника=" + area);
+    }
+
+    public static double triangleArea(double x1, double y1, double x2, double y2, double x3, double y3) {
+        return
+                0.5 * Math.abs((x1 - x2) * (y1 + y2) + (x2 - x3) * (y2 + y3) + (x3 - x1) * (y3 + y1));
     }
 }
