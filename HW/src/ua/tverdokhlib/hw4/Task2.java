8a1961f04dba1e4e4502a45d400f44f622b0d188
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-18 21:26:48 +0200

    HW1

---
+++ HW/src/ua/tverdokhlib/hw4/Task2.java
@@ -0,0 +1,50 @@
+package ua.tverdokhlib.hw4;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = new int[1000];
+        generateRandomArray(array);
+        System.out.println("Original array:");
+        printArray(array);
+        int primeCount = countOfPrimes(array);
+        System.out.println("The count of primes in the array is: " + primeCount);
+    }
+
+    private static int[] generateRandomArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(1, 101);
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.println(value + " ");
+        }
+        System.out.println();
+    }
+
+    private static int countOfPrimes(int[] array) {
+        int primeCount = 0;
+        for (int value : array) {
+            if (isPrime(value)) {
+                primeCount++;
+            }
+        }
+        return primeCount;
+    }
+
+    private static boolean isPrime(int value) {
+        if (value <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % 1 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
