851b915e4f8654081b2244c4a0b61886cbb3edeb
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-19 16:20:14 +0200

    HW1

--- HW/src/ua/tverdokhlib/hw4/Task3.java
+++ HW/src/ua/tverdokhlib/hw4/Task3.java
@@ -1,8 +1,9 @@
 package ua.tverdokhlib.hw4;
+
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Arrays;
 
-public class Task31 {
+public class Task3 {
     public static void main(String[] args) {
         int[] array = new int[2000];
         fillArray(array);
@@ -13,26 +14,29 @@ public class Task31 {
         System.out.println("Массив с замененными на нули четными числами");
         printArray(targetArray);
     }
-    private static void fillArray(int[] array){
+
+    private static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(1, 100);
         }
     }
 
-    public static void replaceEvensWithZeros (int[] array) {
+    private static void replaceEvensWithZeros(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if(array[i] % 2 == 0) {
+            if (array[i] % 2 == 0) {
                 array[i] = 0;
             }
         }
     }
+
     private static int[] copyArray(int[] array) {
-       return Arrays.copyOf(array, array.length);
+        return Arrays.copyOf(array, array.length);
     }
-private static void printArray(int[] array) {
-    for (int i = 0; i < array.length; i++) {
-        System.out.println(array[i] + " ");
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+        System.out.println();
     }
-    System.out.println();
-}
 }
