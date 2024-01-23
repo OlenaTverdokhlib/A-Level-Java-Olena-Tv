fcb3e2b505320104b14e43949896dbd6b9f068e6
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-23 19:07:16 +0200

    HW1

--- HW/src/ua/tverdokhlib/hw4/Task3.java
+++ HW/src/ua/tverdokhlib/hw4/Task3.java
@@ -3,14 +3,15 @@ package ua.tverdokhlib.hw4;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Arrays;
 
+//• Заполнить одномерный массив случайными целочисленными значениями.
+//• Все четные значения заменить на нули. Размер массива - 2000 элементов.
 public class Task3 {
     public static void main(String[] args) {
         int[] array = new int[2000];
         fillArray(array);
         System.out.println("Исходный массив");
         printArray(array);
-        int[] targetArray = copyArray(array);
-        replaceEvensWithZeros(targetArray);
+        int[] targetArray = replaceEvensWithZeros(array);
         System.out.println("Массив с замененными на нули четными числами");
         printArray(targetArray);
     }
@@ -21,16 +22,16 @@ public class Task3 {
         }
     }
 
-    private static void replaceEvensWithZeros(int[] array) {
+    private static int[] replaceEvensWithZeros(int[] array) {
+        int[] targetArray = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                array[i] = 0;
+                targetArray[i] = 0;
+            } else {
+                targetArray[i] = array[i];
             }
         }
-    }
-
-    private static int[] copyArray(int[] array) {
-        return Arrays.copyOf(array, array.length);
+        return targetArray;
     }
 
     private static void printArray(int[] array) {
