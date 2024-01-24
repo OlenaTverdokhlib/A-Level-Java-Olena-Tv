efc249134b453a17b55dd71e571d19d6f3eef916
OlenaTverdokhlib <tverdokhleb.elena@gmail.com>
2024-01-24 21:11:55 +0200

    HW1

--- HW/src/ua/tverdokhlib/hw5/Task3.java
+++ HW/src/ua/tverdokhlib/hw5/Task3.java
@@ -1,4 +1,6 @@
 package ua.tverdokhlib.hw5;
+//Дан двумерный массив NxN(количество строк = количество столбцов), написать программу,
+//которая поменяет местами столбцы и строки.
 
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
@@ -9,8 +11,7 @@ public class Task3 {
         fillArray(sourceArray);
         System.out.println("Исходный массив:");
         printArray(sourceArray);
-        int[][] targetArray = copyArray(sourceArray);
-        int[][] swappedArray = swappedRowsAndColumns(targetArray);
+        int[][] swappedArray = swappedRowsAndColumns(sourceArray);
         System.out.println("Массив после замены местами строк и столбцов:");
         printArray(swappedArray);
     }
@@ -35,15 +36,6 @@ public class Task3 {
         return newArray;
     }
 
-    private static int[][] copyArray(int[][] array) {
-        int rows = array.length;
-        int columns = array[0].length;
-        int[][] copy = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            copy[i] = Arrays.copyOf(array[i], columns);
-        }
-        return copy;
-    }
 
     private static void printArray(int[][] array) {
         for (int[] row : array) {
